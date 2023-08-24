9a6733013e29c746e8ba650471041830a2fb8c3f
sh-kikuchi <shu94hist726@gmail.com>
2023-08-25 02:23:53 +0900

    feature:todo_20230825

--- lib/main.dart
+++ lib/main.dart
@@ -1,148 +1,209 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase_Auth_App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const AuthAppPage(title: 'Firebase_Auth_App'),
+    return const MaterialApp(
+      title: 'ToDo App',
+      home: ToDoPage(),
     );
   }
 }
 
-class AuthAppPage extends StatefulWidget {
-  const AuthAppPage({Key? key, required this.title}) : super(key: key);
-  final String title;
+class ToDoPage extends StatefulWidget {
+  const ToDoPage({Key? key}) : super(key: key);
   @override
-  State<AuthAppPage> createState() => _AuthAppPageState();
+  _ToDoPageState createState() => _ToDoPageState();
 }
 
-class _AuthAppPageState extends State<AuthAppPage> {
-  // Registration Field Email Address
-  String registerUserEmail = "";
-  // Registration Field Password
-  String registerUserPassword = "";
-  // Login field email address
-  String loginUserEmail = "";
-  // Login field password (login)
-  String loginUserPassword = "";
-  // View information about registration and login
-  String DebugText = "";
+class _ToDoPageState extends State<ToDoPage> {
+  final TextEditingController _todoController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+
+  final CollectionReference _todos =
+      FirebaseFirestore.instance.collection('todo');
+  // Functions used for adding and editing
+  Future<void> _add_or_update([DocumentSnapshot? documentSnapshot]) async {
+    String mode = 'addition';
+    if (documentSnapshot != null) {
+      mode = 'update';
+      _todoController.text = documentSnapshot['todo'];
+      _contentController.text = documentSnapshot['content'].toString();
+    }
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                // prevent the soft keyboard from covering text fields
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _todoController,
+                  decoration: const InputDecoration(labelText: 'ToDo'),
+                ),
+                TextField(
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  controller: _contentController,
+                  decoration: const InputDecoration(
+                    labelText: 'Content',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text(mode == 'addition' ? 'Add' : 'Update'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  onPressed: () async {
+                    final String? todo = _todoController.text;
+                    final String? content = _contentController.text;
+                    // Addiing process
+                    if (todo != null && content != null) {
+                      if (mode == 'addition') {
+                        // Persist a new product to Firestore
+                        await _todos.add({"todo": todo, "content": content});
+
+                        // Show the snack bar for the add
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            backgroundColor: Colors.red,
+                            content: Text(
+                              'Added ${todo}!',
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Colors.white),
+                            )));
+                      }
+                      // Editing process
+                      if (mode == 'update') {
+                        // Update the product
+                        await _todos
+                            .doc(documentSnapshot!.id)
+                            .update({"todo": todo, "content": content});
+
+                        // Show the snack bar for the edit
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                                backgroundColor: Colors.red,
+                                content: Text(
+                                  'Updated Content!',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                      backgroundColor: Colors.yellowAccent),
+                                )));
+                      }
+                      // Clear the text fields
+                      _todoController.text = '';
+                      _contentController.text = '';
+
+                      // Hide the bottom sheet
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  // Deletion Processing Functions
+  Future<void> _deleteProduct(String productId) async {
+    await _todos.doc(productId).delete();
+
+    // Show the snack bar for the exclusion
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        backgroundColor: Colors.red,
+        content: Text(
+          'Deleted Content!',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 15, color: Colors.black),
+        )));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                // Set labels for text input
-                decoration: const InputDecoration(labelText: "Mail Address"),
-                onChanged: (String value) {
-                  setState(() {
-                    registerUserEmail = value;
-                  });
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('ToDo App'),
+          backgroundColor: Colors.red,
+        ),
+        // StreamBuilder to pass Firestore values to ListView.builder
+        body: StreamBuilder(
+          stream: _todos.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              // Display FireStore values in list format
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                      streamSnapshot.data!.docs[index];
+                  // View documents in the card widget
+                  return Card(
+                    margin: const EdgeInsets.all(10),
+                    child: ListTile(
+                      title: Text(documentSnapshot['todo']), // ToDo
+                      subtitle: Text(
+                          documentSnapshot['content'].toString()), // Content
+                      trailing: SizedBox(
+                        width: 100,
+                        child: Row(
+                          children: [
+                            // Edit Button
+                            IconButton(
+                                color: Colors.red,
+                                icon: const Icon(Icons.edit),
+                                onPressed: () =>
+                                    _add_or_update(documentSnapshot)),
+                            // Delete Button
+                            IconButton(
+                                color: Colors.red,
+                                icon: const Icon(Icons.delete),
+                                onPressed: () =>
+                                    _deleteProduct(documentSnapshot.id)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
                 },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    labelText: "6 character long Password"),
-                // Mask not to show password
-                obscureText: true,
-                onChanged: (String value) {
-                  setState(() {
-                    registerUserPassword = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 1),
-              ElevatedButton(
-                onPressed: () async {
-                  try {
-                    // User Registration
-                    final FirebaseAuth auth = FirebaseAuth.instance;
-                    final UserCredential result =
-                        await auth.createUserWithEmailAndPassword(
-                      email: registerUserEmail,
-                      password: registerUserPassword,
-                    );
-                    // Registered User Information
-                    final User user = result.user!;
-                    setState(() {
-                      DebugText = "Register OK：${user.email}";
-                    });
-                  } catch (e) {
-                    // Failed User Information
-                    setState(() {
-                      DebugText = "Register Fail：${e.toString()}";
-                    });
-                  }
-                },
-                child: const Text("User Registration"),
-              ),
-              const SizedBox(height: 1),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Mail Address"),
-                onChanged: (String value) {
-                  setState(() {
-                    loginUserEmail = value;
-                  });
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Password"),
-                obscureText: true,
-                onChanged: (String value) {
-                  setState(() {
-                    loginUserPassword = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 1),
-              ElevatedButton(
-                onPressed: () async {
-                  try {
-                    // Try login
-                    final FirebaseAuth auth = FirebaseAuth.instance;
-                    final UserCredential result =
-                        await auth.signInWithEmailAndPassword(
-                      email: loginUserEmail,
-                      password: loginUserPassword,
-                    );
-                    // Succeeded to login
-                    final User user = result.user!;
-                    setState(() {
-                      DebugText = "Succeeded to Login：${user.email}";
-                    });
-                  } catch (e) {
-                    // Failed to login
-                    setState(() {
-                      DebugText = "Failed to Login：${e.toString()}";
-                    });
-                  }
-                },
-                child: const Text("Login"),
-              ),
-              const SizedBox(height: 8),
-              Text(DebugText),
-            ],
-          ),
+              );
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+        // Add Button
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red,
+          onPressed: () => _add_or_update(),
+          child: const Icon(Icons.add),
         ),
       ),
     );
